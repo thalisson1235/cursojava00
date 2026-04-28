bbd79651c405cd67b5a38914af74d5eb9b053d8a
thalisson <thalisson1240@gmail.com>
2026-04-27 22:39:24 -0300

    aula array01

---
+++ src/main/java/arrays/array01.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class array01 {
+    public static void main(String[] args) {
+        int [] idades = new int[3];
+        idades[0] = 21;
+        idades[1] = 53;
+        idades[2] = 17;
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
