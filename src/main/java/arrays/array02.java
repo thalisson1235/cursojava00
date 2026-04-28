422c788debb80a81f33437695cc15eeef4eca1f9
thalisson <thalisson1240@gmail.com>
2026-04-27 22:41:09 -0300

    aula array02

---
+++ src/main/java/arrays/array02.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class array02 {
+    public static void main(String[] args) {
+        String [] nomes = new String[4];
+
+        nomes[0] = "Carlos";
+        nomes[1] = "vini";
+        nomes[2] = "rum";
+        nomes[3] = "thalisson";
+
+
+        for (int i = 0; i<nomes.length; i++){
+            System.out.println(nomes[i]);
+        }
+    }
+}
