afd8a0c3a6f6b6d6651feaa1a140e90013c76ef5
thalisson <thalisson1240@gmail.com>
2026-04-27 22:43:55 -0300

    aula array05

---
+++ src/main/java/arrays/array05.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class array05 {
+    public static void main(String[] args) {
+        int[] array = {1,2,3};
+
+        int [][] arrayInt = new int[3][];
+
+        arrayInt[0] = new int[2];
+        arrayInt[1] = array;
+        arrayInt[2] = new int[]{1,2,3,4,5,6};
+
+        int[][] arrayInt2 = {{0,1}, {0,1,2}, {0,1,2,3,4,5}};
+
+        for (int[] arrayBase : arrayInt2) {
+            for(int num : arrayBase){
+                System.out.println(num);
+            }
+        }
+    }
+}
